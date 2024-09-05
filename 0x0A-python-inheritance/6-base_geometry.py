126b67daf34c86d217efb23f583eb327348b68eb
Eman-salem123 <emanzatonaa@gmail.com>
2024-09-05 19:51:35 +0300

    lol

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,38 +1,13 @@
 #!/usr/bin/python3
-""" 5-base_geometry module """
+""" creating a class """
 
 
 class BaseGeometry:
-
     """
-    A base class for geometric shapes.
-
-    This class provides common functionality and properties
-    that are shared among different geometric shapes.
-    Subclasses are expected to implement specific methods
-     for their respective shapes.
-
-    Attributes:
-    None
-
+    BaseGeometry class
     """
-
     def area(self):
         """
-    Calculate the area of the geometric shape.
-
-    This method calculates the area of the geometric shape.
-    Since it's a placeholder method
-    and not implemented in this base class,
-    it raises an Exception to indicate that it needs
-    to be implemented in subclasses.
-
-    Raises:
-        Exception: This method is not implemented in 
-        the base class and must be overridden
-                   in subclasses.
-
-    Returns:
-        None
-    """
+        area method
+        """
         raise Exception("area() is not implemented")
