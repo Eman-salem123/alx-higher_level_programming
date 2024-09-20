f8a2360669295db84954a7dc28db11f270217eb1
Eman-salem123 <emanzatonaa@gmail.com>
2024-09-20 23:34:27 +0300

    lol

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
+
 exports.esrever = function (list) {
-    return list.reduceRight(function (array, current) {
-      array.push(current);
-      return array;
-    }, []);
-  };
+  const newArr = [];
+  for (let x = list.length - 1; x >= 0; x--) {
+    newArr.push(list[x]);
+  }
+  return (newArr);
+};
